9dfea8ece1c9d01cf466b7d6f2ccb941dc023a36
Pavel John <pavelj@salsitasoft.com>
2019-12-04 09:55:50 +0100

    On rendering collapsible html

--- src/index.js
+++ src/index.js
@@ -3,89 +3,107 @@ import path from 'path';
 import { initConfig, getConfig } from './config';
 import * as FSUtils from './fsUtils';
 
+import ReactDOMServer from 'react-dom/server';
+
 const buildFormatFile = (linkPrefix, treeOffset) => fileName => {
   const trimmedFileName = getConfig('trimExtension') ? FSUtils.trimExtension(fileName) : fileName;
   return `${treeOffset}  - [${trimmedFileName}](${linkPrefix}/${trimmedFileName})\n`;
 };
 
-const formatRoot = (hasRootMd, treeOffset, depth, rootName, linkPrefix) => {
-  const rootBullet = depth > 0 ? '- ' : '';
-  const fileName = depth === 0
-    ? FSUtils.trimExtension(getConfig('rootFileName'))
-    : rootName;
-  return (hasRootMd)
-    ? `${treeOffset}${rootBullet}[${rootName}](${linkPrefix}/${fileName})\n`
-    : `${treeOffset}${rootBullet}${rootName}\n`;
-};
-
 const formatTree = (tree, linkPrefix, depth) => {
   const treeOffset = ' '.repeat(depth * 2);
-  let string = formatRoot(tree.hasRootMd, treeOffset, depth, tree.rootName, linkPrefix);
+  const collapsible = tree.childDirs.length + tree.childFiles.length > 0;
+  let string = formatDirectory(tree.hasRootMd, treeOffset, depth, tree.rootName, linkPrefix, collapsible);
 
-  if (tree.childDirs.length > 0) {
-    string = string.concat(tree.childDirs.join(''));
-  }
+
+  const bulletStart = depth > 0 ? '<li> ' : '';
+  const bulletEnd = depth > 0 ? '</li> ' : '';
+  const fileName = depth === 0
+    ? FSUtils.trimExtension(getConfig('rootFileName'))
+    : tree.rootName;
+  const treeTitle = tree.hasRootMd
+    ? `<a href="${linkPrefix}/${fileName}">${rootName}</a>`
+    : rootName;
+
+  const childDirs = string.concat(tree.childDirs.join(''));
+
+  const formatFile = buildFormatFile(linkPrefix, treeOffset);
+  const childFilesMd = tree.childFiles.map(formatFile);
+  const childFiles = string.concat(childFilesMd.join(''));
+
+  const collapsibleEnd = collapsible
+    ? '</details>'
+    : '';
+
+  const subTree = collapsible
+    ? `${treeOffset}${bulletStart}<details><summary>${treeTitle}</summary> \n${treeOffset}${bulletEnd}`
+    : `${treeOffset}${rootBullet}[${rootName}](${linkPrefix}/${fileName})\n`;
+
 
   if (tree.childFiles.length > 0) {
-    const formatFile = buildFormatFile(linkPrefix, treeOffset);
-    const childFilesMd = tree.childFiles.map(formatFile);
-    string = string.concat(childFilesMd.join(''));
-  }
-  return string;
-};
 
-const mdFileTree = async (dirPath, linkPrefix, depth = 0) => {
-  if (depth > getConfig('maxDepth')) {
-    return null;
-  }
 
-  const tree = {
-    rootName: path.basename(dirPath),
-    rootPath: path.resolve(dirPath),
-    hasRootMd: false,
-    childDirs: [],
-    childFiles: [],
+    if (collapsible) {
+      string = string + '</p>\n</details>';
+    }
+
+    return string;
   };
-  const children = await FSUtils.readdirPromise(dirPath);
 
-  await Promise.all(children.map(async child => {
-    const childPath = path.resolve(dirPath, child);
-    if (getConfig('exclude').some(exclude => exclude.test(childPath))) {
-      return;
+  const mdFileTree = async (dirPath, linkPrefix, depth = 0) => {
+    if (depth > getConfig('maxDepth')) {
+      return null;
     }
-    const childStats = await FSUtils.statPromise(childPath);
-    if (childStats.isDirectory()) {
-      const childLinkPrefix = `${linkPrefix}`;
-      const childDir = await mdFileTree(childPath, childLinkPrefix, depth + 1);
-      if (childDir !== null) {
-        tree.childDirs.push(childDir);
+
+    const tree = {
+      rootName: path.basename(dirPath),
+      rootPath: path.resolve(dirPath),
+      hasRootMd: false,
+      childDirs: [],
+      childFiles: [],
+    };
+    const children = await FSUtils.readdirPromise(dirPath);
+
+    await Promise.all(children.map(async child => {
+      const childPath = path.resolve(dirPath, child);
+      if (getConfig('exclude').some(exclude => exclude.test(childPath))) {
+        return;
       }
-    } else if (
-      depth === 0 && getConfig('rootFileName') === child ||
-      tree.rootName === FSUtils.trimExtension(child)
-    ) {
-      tree.hasRootMd = true;
-    } else if (getConfig('include').some(include => include.test(child))) {
-      tree.childFiles.push(child);
+      const childStats = await FSUtils.statPromise(childPath);
+      if (childStats.isDirectory()) {
+        const childLinkPrefix = `${linkPrefix}`;
+        const childDir = await mdFileTree(childPath, childLinkPrefix, depth + 1);
+        if (childDir !== null) {
+          tree.childDirs.push(childDir);
+        }
+      } else if (
+        depth === 0 && getConfig('rootFileName') === child ||
+        tree.rootName === FSUtils.trimExtension(child)
+      ) {
+        tree.hasRootMd = true;
+      } else if (getConfig('include').some(include => include.test(child))) {
+        tree.childFiles.push(child);
+      }
+      return;
+    }));
+
+    return (tree.hasRootMd || tree.childDirs.length + tree.childFiles.length > 0)
+      ? formatTree(tree, linkPrefix, depth)
+      : null;
+  };
+
+  const run = async configPath => {
+    try {
+      ReactDOMServer.renderToStaticMarkup();
+
+      await initConfig(configPath || '.mftrc.json');
+      const dirPath = path.resolve(FSUtils.resolveHome(getConfig('source')));
+      const tree = await mdFileTree(dirPath, getConfig('linkPrefix'));
+      const outputPath = path.resolve(FSUtils.resolveHome(getConfig('output')));
+      await FSUtils.writeFilePromise(outputPath, tree);
+    } catch (error) {
+      console.log(error);
     }
-    return;
-  }));
+  };
 
-  return (tree.hasRootMd || tree.childDirs.length + tree.childFiles.length > 0)
-    ? formatTree(tree, linkPrefix, depth)
-    : null;
-};
-
-const run = async configPath => {
-  try {
-    await initConfig(configPath || '.mftrc.json');
-    const dirPath = path.resolve(FSUtils.resolveHome(getConfig('source')));
-    const tree = await mdFileTree(dirPath, getConfig('linkPrefix'));
-    const outputPath = path.resolve(FSUtils.resolveHome(getConfig('output')));
-    await FSUtils.writeFilePromise(outputPath, tree);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-run(process.argv[2]);
+  run(process.argv[2]);
